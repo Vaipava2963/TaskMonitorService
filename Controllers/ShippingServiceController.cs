17e0e569507b705a516344b6ddecd1dcdff6eb20
Kavya <kavya.subramani@relevantz.com>
2025-08-01 15:05:35 +0530

    Add files via upload

---
+++ Controllers/ShippingServiceController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShippingServices.Data;
+using ShippingServices.Model;
+
+namespace ShippingServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShippingServiceController : ControllerBase
+    {
+        private readonly AppDBContext _Context;
+        public ShippingServiceController(AppDBContext context)
+        {
+            _Context = context;
+        }
+        //Get: api/
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var shipments= await _Context.ShippingItems.ToListAsync();
+            return Ok(shipments);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var shipment=await _Context.ShippingItems.FindAsync(id);
+            if (shipment == null) return NotFound();
+            else return Ok(shipment);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ShippingItem shippingItem)
+        {
+            _Context .ShippingItems.Add(shippingItem);
+            await _Context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Create), shippingItem);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ShippingItem shippingItem)
+        {
+            var shipment = await _Context.ShippingItems.FindAsync(id);
+            if (shipment == null) return NotFound(); 
+           shipment.ItemDescription = shippingItem.ItemDescription;
+            shipment.ItemName = shippingItem.ItemName;
+            shipment.ItemDescription = shippingItem.ItemDescription;
+            shipment.ItemShippedDate = shippingItem.ItemShippedDate;
+            await _Context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var shipment = await _Context.ShippingItems.FindAsync(id);
+            if (shipment == null) return NotFound();
+            _Context.ShippingItems.Remove(shipment);
+            await _Context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+    }
+}
---
+++ Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShippingServices.Model;
+namespace ShippingServices.Data
+
+
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options) { }
+        public DbSet<ShippingItem> ShippingItems { get; set; }  
+    }
+}
